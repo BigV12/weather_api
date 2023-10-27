ea50e9c6a689acfce47d3b4f168bb5260420d639
bigv12 <duruewuruikenna12@gmail.com>
2023-10-27 22:43:12 +0100

    remove uncessary consol.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("this is ;loading ");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
